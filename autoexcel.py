d0bfba30609ae4c2cbaade5b9a62f8799ffcd386
ufo-8 <unidentified.treasure.64@gmail.com>
2022-03-31 18:05:24 +0900

    my 4th commit

--- autoexcel.py
+++ autoexcel.py
@@ -1,29 +1,23 @@
 import openpyxl as px
 import glob
 
-# ====== (1) ファイル名から情報を抜き出す ======
+# ====== (1) 参照元・出力先のファイル設定 ======
 
 # フォルダ内にあるファイル名を取得
 files = glob.glob("*交通費請求明細*.xlsx")
 for file in files:
     file_name = file
 
-#import_sheet_name = '交通費_4月'
-#export_file_path = 'YYYY年MM月度_交通費一覧.xlsx'
-#export_sheet_name = '一覧'
-
-# Pandasのread_excel関数でExcelファイルを読み込む
-#df_order = pd.read_excel(import_file, sheet_name=import_file_ws)
-
-
-# 参照先のファイル・シート指定
+# 参照元のファイル・シート指定
 import_file = px.load_workbook(file_name, keep_vba=True)
 import_file_ws = import_file.worksheets[2]
 
-# コピーするファイル・シート指定
+# 出力先のファイル・シート指定
 wb = px.load_workbook('YYYY年MM月度_交通費一覧.xlsx')
 ws = wb.worksheets[0]
 
+
+# ====== (2) 参照元ファイル名から情報を抜き出す ======
 
 l = file_name
 
@@ -54,7 +48,7 @@ ws.cell(row=2, column=2, value=employee_num)
 ws.cell(row=2, column=3, value=employee_name)
 
 
-# ====== (2) ファイル内の情報を他ファイルへコピーする ======
+# ====== (3) 参照元ファイル内の情報を出力先ファイルへコピーする ======
 
 # （19,1) 月日をコピー
 cell_value = import_file_ws.cell(row=19, column=1).value
